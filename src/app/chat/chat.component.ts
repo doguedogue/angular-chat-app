650d730d51f0db1b852290c61c87b05297f3400c
Rafael Vilches <rvilches38@yahoo.com.mx>
2023-03-21 01:01:57 -0600

    add suscriber event and publish messages

--- src/app/chat/chat.component.ts
+++ src/app/chat/chat.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Client } from '@stomp/stompjs';
 import * as SockJS from 'sockjs-client';
+import { Mensaje } from './model/mensaje';
 
 @Component({
   selector: 'app-chat',
@@ -11,6 +12,9 @@ export class ChatComponent implements OnInit {
 
   private client: Client;
   public conectado: boolean = false;
+
+  mensaje: Mensaje = new Mensaje();
+  mensajes: Mensaje[] = [];
 
   constructor() { }
 
@@ -23,6 +27,13 @@ export class ChatComponent implements OnInit {
     this.client.onConnect = (frame) => {
       console.log('Conectados: ' + this.client.connected + " : " + frame);
       this.conectado = true;
+
+      this.client.subscribe('/chat/mensaje', e => {
+        let mensaje: Mensaje = JSON.parse(e.body) as Mensaje;
+        mensaje.fecha = new Date(mensaje.fecha);
+        this.mensajes.push(mensaje);
+        console.log(mensaje);
+      });
     }
 
     this.client.onDisconnect = (frame) => {
@@ -37,5 +48,15 @@ export class ChatComponent implements OnInit {
 
   desconectar(): void{
     this.client.deactivate();
+  }
+
+  enviarMensaje(): void{
+    if (this.mensaje.texto.trim() == '')
+      return;
+    this.client.publish({
+      destination: '/app/mensaje',
+      body: JSON.stringify(this.mensaje)
+    });
+    this.mensaje.texto = '';
   }
 }
